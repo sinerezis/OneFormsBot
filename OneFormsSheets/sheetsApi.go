ecc2e433752a1693186265a575f93f6b8823d017
sinerezis <y0mi@yandex.ru>
2022-08-24 12:37:03 +0400

    Продолжаем рефакторинг)

--- OneFormsSheets/sheetsApi.go
+++ OneFormsSheets/sheetsApi.go
@@ -16,7 +16,7 @@ import (
 // Инициализируем доступ к таблице
 func StartSheet(sheetUrl string) (*spreadsheet.Sheet, error) {
 
-	data, err := ioutil.ReadFile("OneForms/google-credentials.json")
+	data, err := ioutil.ReadFile("google-credentials.json")
 	if err != nil {
 		return nil, err
 	}
