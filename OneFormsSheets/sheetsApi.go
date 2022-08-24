b56634ca55455b552791f06173d72f5913bfc543
sinerezis <y0mi@yandex.ru>
2022-08-25 00:48:31 +0400

    v0.4.1

--- OneFormsSheets/sheetsApi.go
+++ OneFormsSheets/sheetsApi.go
@@ -93,6 +93,7 @@ func CheckSheet(sheet *spreadsheet.Sheet) ([]string, error) {
 		if counter > countOfRows {
 			for ; countOfRows <= counter; countOfRows++ {
 				orders = append(orders, sheet.Rows[countOfRows][3].Value)
+				log.Print(sheet.Rows[countOfRows][3].Value)
 
 				// Обновляем кол-во прочитанных заказов, записаное
 				// в таблице
