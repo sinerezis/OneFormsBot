fda26b0d0c53aeaedffc44c94fcfe02c7a9107d9
sinerezis <y0mi@yandex.ru>
2022-08-25 18:10:16 +0400

    v0.4.2

--- Cmd/main.go
+++ Cmd/main.go
@@ -50,9 +50,9 @@ func SendOrders(sheetUrl string) error {
 func main() {
 	var wg sync.WaitGroup
 
-	wg.Add(3)
+	wg.Add(2)
 
-	go http.HandleFunc("/", bot.MainHandler)
+	http.HandleFunc("/", bot.MainHandler)
 	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	go SendOrders(token.SheetURL)
 
